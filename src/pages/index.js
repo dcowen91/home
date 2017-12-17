a58824e822f0c49ee7b6b1224f50db37ee54495e
Drew Owen <anowen@microsoft.com>
2017-12-16 23:01:02 -0800

    change bg

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1><span style={{color:"pink"}}className="prefix">dc</span><span>owen</span></h1>
+    <h1><span style={{color:"#e05d44"}}className="prefix">dc</span><span>owen</span></h1>
   </div>
 )
 
