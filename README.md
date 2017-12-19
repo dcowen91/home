b2c1a4ea02fb061c00afeeaf1401217c901af753
Drew Owen <anowen@microsoft.com>
2017-12-19 00:11:57 -0800

    some changes

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # home
 
 my homepage
+
+# TODO
+* Typescript
+* Add prettier, husky, lint-staged
+* Add scss
+* delete unused styles, pages
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,37 @@
 import React from "react";
 import Link from "gatsby-link";
+import {FaTwitterSquare, FaGithubSquare, FaLinkedinSquare, FaEnvelopeSquare} from "react-icons/lib/fa";
+
 
 const IndexPage = () => (
   <div>
-    <div className="headerContainer">
-      <h1>
-        <span className="prefix">
-          dc
-        </span>
-        <span>owen</span>
-      </h1>
+    <div className="hero">
+      <div className="headerContainer">
+        <h1>
+          <span className="prefix">
+            dc
+          </span>
+          <span>owen</span>
+        </h1>
+      </div>
     </div>
-    <div className="content"
-    >
-    <h2> About </h2>
-    {/* TODO */ }
-    <p> Software Engineer </p>
-    <p> Coding </p>
-    <p> Coding </p>
-    <p> Coding </p>
+    <div className="content">
+    <div className="nav">
+        <Link to="/">Home</Link>
+        <Link to="/">Blog</Link>
+        <Link to="/">Project</Link>
+    </div>
+    <h2>Drew Owen</h2>
+    <p>coding</p>
+    <p>sports</p>
+    <p>esports</p>
+    <p>etc</p>
+    <div className="social">
+        <FaLinkedinSquare />
+        <FaGithubSquare />
+        <FaTwitterSquare />
+        <FaEnvelopeSquare />
+    </div>
     </div>
   </div>
 );
