bdf1d6e0d760b9056485f38973fb54053ef89fbe
Drew Owen <dcowen91@gmail.com>
2021-02-05 18:09:58 -0800

    start next.js proj

---
+++ _old/README.md
@@ -0,0 +1,15 @@
+# Home
+
+My personal portfolio website.
+
+[dcowen.co](https://dcowen.co)
+
+# TODO
+
+-   [x] Convert to gatsby v2
+-   [x] Add Typescript
+-   [x] convert .css to emotion
+-   [x] Add tag functionality
+-   [x] split out components
+-   [ ] Refresh layout/styles
+-   [ ] Blog more
---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Henlo</h1>
+
+        <p className={styles.description}>
+          Get started by editing{" "}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
+
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3>Greetings</h3>
+            <p>Suppppppp.</p>
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
